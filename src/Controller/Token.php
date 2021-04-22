077f0787b0f46b5ddab81a5fc1a2fef490118d54
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-04-22 15:36:59 +0200

    refactor: Add a LocalEuLoginApiCredentials service to be used for testing.

--- src/Controller/Token.php
+++ src/Controller/Token.php
@@ -1,29 +1,36 @@
 <?php
 
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace EcPhp\EuLoginApiAuthenticationBundle\Controller;
 
+use Firebase\JWT\JWT;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 final class Token
 {
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
-        $username = uniqid('user_');
-
-        $payload = [
-            'iat' => time(),
-            'sub' => $username,
-            'jti' => uniqid(),
-            'iss' => '/api/token',
-            'foo' => 'bar',
-        ];
-
         return new JsonResponse(
             [
-                'token' => false,
-            ]
+                'token' => JWT::encode(
+                    [
+                        'at' => JWT::encode(
+                            (array) json_decode($request->getContent(), true) +
+                            ['sub' => uniqid('user_'), 'active' => true],
+                            uniqid()
+                        ),
+                    ],
+                    uniqid()
+                ),
+            ],
+            200
         );
     }
 }
---
+++ src/Service/LocalEuLoginApiCredentials.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace EcPhp\EuLoginApiAuthenticationBundle\Service;
+
+use EcPhp\EuLoginApiAuthenticationBundle\Exception\EuLoginApiAuthenticationException;
+use Psr\Http\Message\RequestInterface;
+use Throwable;
+
+use function array_key_exists;
+use function count;
+use function is_object;
+
+final class LocalEuLoginApiCredentials implements EuLoginApiCredentialsInterface
+{
+    private EuLoginApiCredentialsInterface $euLoginApiCredentials;
+
+    public function __construct(EuLoginApiCredentialsInterface $euLoginApiCredentials)
+    {
+        $this->euLoginApiCredentials = $euLoginApiCredentials;
+    }
+
+    public function getCredentials(RequestInterface $request): array
+    {
+        // 0. Get the pop token from the request.
+        // 1. Decode the payload of the pop token.
+        // 2. Get the AT Token from the payload and submit it for introspection to EU Login.
+        // 3. From the response, get the public key and verify the pop token signature.
+        // 4. Return the response claims if the signature match.
+
+        // Step 0.
+        try {
+            $token = $this->getPopToken($request);
+        } catch (Throwable $e) {
+            throw EuLoginApiAuthenticationException::unableToGetCredentials($e);
+        }
+
+        // Step 1.
+        try {
+            $accessToken = $this->getAccessToken($token);
+        } catch (Throwable $e) {
+            throw EuLoginApiAuthenticationException::unableToGetCredentials($e);
+        }
+
+        // Step 2.
+        [, $payload,] = array_map(
+            'json_decode',
+            array_map(
+                'base64_decode',
+                explode(
+                    '.',
+                    $accessToken,
+                    3
+                )
+            )
+        );
+        $response = (array) $payload;
+
+        if (false === array_key_exists('active', $response)) {
+            throw EuLoginApiAuthenticationException::unableToGetCredentials(
+                EuLoginApiAuthenticationException::invalidIntrospectionEndpointResponse()
+            );
+        }
+
+        if (false === $response['active']) {
+            throw EuLoginApiAuthenticationException::unableToGetCredentials(
+                EuLoginApiAuthenticationException::invalidOrRevokedToken()
+            );
+        }
+
+        // Step 3 is skipped.
+
+        // Step 4.
+        return (array) $payload;
+    }
+
+    public function hasPopToken(RequestInterface $request): bool
+    {
+        return $this->euLoginApiCredentials->hasPopToken($request);
+    }
+
+    private function getAccessToken(string $popToken): string
+    {
+        try {
+            [, $payload,] = array_map(
+                'json_decode',
+                array_map(
+                    'base64_decode',
+                    explode(
+                        '.',
+                        $popToken,
+                        3
+                    )
+                )
+            );
+        } catch (Throwable $e) {
+            throw EuLoginApiAuthenticationException::unableToGetAccessTokenFromPopToken($popToken, $e);
+        }
+
+        if (is_object($payload) && property_exists($payload, 'at')) {
+            return $payload->at;
+        }
+
+        throw EuLoginApiAuthenticationException::unableToGetAccessTokenFromPopToken($popToken);
+    }
+
+    private function getPopToken(RequestInterface $request): string
+    {
+        if (false === $request->hasHeader('Authorization')) {
+            throw EuLoginApiAuthenticationException::noAuthorizationHeaderInRequest($request);
+        }
+
+        $headerParts = explode(' ', $request->getHeaderLine('Authorization'));
+
+        if (2 !== count($headerParts)) {
+            throw EuLoginApiAuthenticationException::invalidAuthorizationHeader($headerParts);
+        }
+
+        if ('pop' !== $headerParts[0]) {
+            throw EuLoginApiAuthenticationException::invalidAuthorizationHeaderPrefix($headerParts[0]);
+        }
+
+        if (3 !== count(explode('.', $headerParts[1]))) {
+            throw EuLoginApiAuthenticationException::invalidJWTTokenStructure($headerParts[1]);
+        }
+
+        return $headerParts[1];
+    }
+}
